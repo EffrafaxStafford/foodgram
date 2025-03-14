53ee89ccd2b9cf0f6e8c6495da718317d5adf1c8
Abas Lampezhev <abas.lampezhev@gmail.com>
2025-03-14 17:37:42 +0300

    added csv_to_db.py

---
+++ backend/foodgram/csv_to_db.py
@@ -0,0 +1,29 @@
+import csv
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram.settings')
+
+application = get_wsgi_application()
+
+from recipes.models import Ingredients
+
+
+def read_csv(filename):
+    """Считывает данные из csv и возвращает список строк таблицы."""
+    path = os.path.join('../../data', filename)
+    with open(path, encoding='utf-8') as csv_file:
+        reader = csv.reader(csv_file)
+        return list(reader)
+
+
+def load_ingredients_data():
+    """Загрузка данных в модель Ingredients."""
+    filename = 'ingredients.csv'
+    for row in read_csv(filename):
+        Ingredients.objects.get_or_create(name=row[0], measurement_unit=row[1])
+
+
+if __name__ == '__main__':
+    load_ingredients_data()
